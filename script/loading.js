f2f696f6bb87f18943668630a60fa6c4236bf409
peterTanos <tanospeter@gmail.com>
2022-02-27 19:10:58 +0100

    demo

---
+++ script/loading.js
@@ -0,0 +1,6 @@
+setTimeout(loading, 1700)
+
+function loading(){
+  document.getElementById("loading").style.visibility = "hidden";
+  document.getElementById("meta-table").style.visibility = "visible";
+}
